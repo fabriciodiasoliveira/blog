8d31041abf1b1e541c560236df0f5f6108bf8cc0
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-06-09 09:16:02 -0300

    Inseridos os helpers da aplicação

--- resources/views/components/helpers/headers.blade.php
+++ resources/views/components/helpers/headers.blade.php
@@ -3,19 +3,26 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>-->
 <!-- js -->
-<script src="{{ asset('js/jquery-3.2.1.slim.min.js') }}"></script>
-<script src="{{ asset('js/jquery.mask.min.js') }}"></script>
-<script src="{{ asset('js/popper.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/global.js') }}"></script>
-<!--css -->
-<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}"/>
-<link rel="stylesheet" type="text/css" href="{{ asset('css/animate.min.css') }}"/>
-<link rel="stylesheet" type="text/css" href="{{ asset('css/global.css') }}"/>
-<!-- Scripts -->                                         
-<script src="{{ asset('js/app.js') }}" defer></script> 
-<!-- Fonts -->
-<link rel="dns-prefetch" href="//fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-<!-- Styles -->
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Personalizações -->
+    <link href="{{ asset('css/global.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/global.js') }}" defer></script>
