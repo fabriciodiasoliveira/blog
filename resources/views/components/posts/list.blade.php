4825faa16ec1ea959e055babef2d966e0b43b8f1
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-06-13 18:04:14 -0300

    Conclusão da tela inicial com as postagens

--- resources/views/components/posts/list.blade.php
+++ resources/views/components/posts/list.blade.php
@@ -3,13 +3,13 @@
     <div class="col-md-12">
         <table class="table table-striped" width="100%">
             @foreach ($posts as $post)
-            <tr>
-                <td> {{ $post->head }}</td>
-            </tr>
-            <tr>
-                
-                <td{{ $post->summary }}</td>
-            </tr>
+            <div class="card">
+                <div class="card-header">{{ $post->head }}</div>
+
+                <div class="card-body">
+                    {{ $post->summary }}
+                </div>
+            </div>
             @endforeach
         </table>
     </div>
