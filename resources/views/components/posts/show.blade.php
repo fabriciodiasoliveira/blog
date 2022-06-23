13c28f05da9886c3007b7dcfa8dac6331efc2b3a
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-06-23 10:29:26 -0300

    Mudança na formatação final das exibições

--- resources/views/components/posts/show.blade.php
+++ resources/views/components/posts/show.blade.php
@@ -5,8 +5,8 @@
                 <div class="card-header"><h1>{{ $post->head }}</h1></div>
 
                 <div class="card-body">
-                    <h2>{!! nl2br($post->summary) !!}</h2>
-                    {!! nl2br($post->body) !!}
+                    <h2>{!! nl2br(e($post->summary)) !!}</h2>
+                    {!! $post->body !!}
                 </div>
             </div>
         </div>
