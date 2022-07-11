e5c2cae2c67b4dac2d05c15e3f79ae99be698473
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-11 18:40:47 -0300

    Retorno ao tema padrão

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="{{ asset('images/logo.png') }}"/> {{ config('app.name', 'Laravel') }}
