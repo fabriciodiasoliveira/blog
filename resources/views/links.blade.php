842621fbff79ac6539ec3d6a0901c3f5f7f9ec7f
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-12 16:19:48 -0300

    Mudança nos links

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-md-12">
-        <a href="http://rpg.fabricioweb.net">Mundo sombrio</a>
+        <a href="http://rpg.fabricioweb.net">Mundo sombrio - site de RPG</a>
     </div>
 </div>
 <div class="row">
