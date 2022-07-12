435919ae73891a402185d8ea6780f39ef8897595
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-12 15:58:18 -0300

    Correção do alinhamento da exibição da postagem

--- resources/views/components/posts/show.blade.php
+++ resources/views/components/posts/show.blade.php
@@ -1,14 +1,12 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header"><h1>{{ $post->head }}</h1></div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header"><h1>{{ $post->head }}</h1></div>
 
-                <div class="card-body">
-                    <h2>{!! $post->summary !!}</h2>
-                    {!! $post->body !!}
-                    <div class="text-end"><small><i>{{ $post->name }}</i></small></div>
-                </div>
+            <div class="card-body">
+                <h2>{!! $post->summary !!}</h2>
+                {!! $post->body !!}
+                <div class="text-end"><small><i>{{ $post->name }}</i></small></div>
             </div>
         </div>
     </div>
