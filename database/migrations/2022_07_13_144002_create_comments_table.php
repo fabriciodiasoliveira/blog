fc7affe23051a37e8a23b340daa83d2b80822e2c
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-13 11:43:00 -0300

    Alterações de layout pontuais para melhor exibição

--- database/migrations/2022_07_13_144002_create_comments_table.php
+++ database/migrations/2022_07_13_144002_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePermissionsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreatePermissionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_permissions', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('permission')->default('noadmin');
-            $table->foreignId('user_id')->constrained('users')->restrictOnDelete();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreatePermissionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('permissions');
+        Schema::dropIfExists('comments');
     }
 }
--- resources/views/components/posts/list.blade.php
+++ resources/views/components/posts/list.blade.php
@@ -1,35 +1,47 @@
 
 @php
- $col = array (
-  array(3,3,6),
-  array(6,3,3),
-  array(2,2,8),
-  array(4,4,4),
+$col = array (
+array(3,3,6),
+array(6,3,3),
+array(2,2,8),
+array(8,2,2),
+array(2,8,2),
+array(2,4,2),
+array(6,3,2),
 );
- $x = 0;
- $y = rand(0,3);
+$x = 0;
+$y = rand(0,3);
 @endphp
 <div class="row">
-@foreach ($posts as $post)
+    @foreach ($posts as $post)
     <div class="col-md-{{$col[$y][$x]}}">
-        <div class="card">
-            <div class="card-header"><a href="{{ route('post.show.visit', $post->id) }}"><h2>{{ $post->head }}</h2></a></div>
-
-            <div class="card-body">
-                {!! $post->summary !!}
-                <div class="text-end"><small><i>{{ $post->name }}</i></small> - <i> Adicionado em {{ date('d/m/Y - H:i:s', strtotime($post->created_at)) }}</i></small></div>
-                @php $x++; @endphp
+        <div class="row">
+            <div class="col-md-12" style="
+                 background-color: #dddddd;
+                 border-radius: 25px;">
+                <a href="{{ route('post.show.visit', $post->id) }}"><h2>{{ $post->head }}</h2>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-12">
+                {!! $post->summary !!}</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-end"><small><i>{{ $post->name }}</i></small> - <i> Adicionado em {{ date('d/m/Y - H:i:s', strtotime($post->created_at)) }}</i></small></div>
+            </div>
+        </div>
+        @php $x++; @endphp
     </div>
-@if($x == 3)
+    @if($x == 3)
 </div>
 <div class="row">
-@php $x = 0; $y = rand(0,count($col)-1); @endphp
-@endif
-@endforeach
+    @php $x = 0; $y = rand(0,count($col)-1); @endphp
+    @endif
+    @endforeach
 </div>
 
 <div class="d-flex">
-          {!! $posts->links('pagination::bootstrap-4') !!}
+    {!! $posts->links('pagination::bootstrap-4') !!}
 </div>
