eb6f800bc47f2e785b547c95a8eac5aad44634a9
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-18 10:43:21 -0300

    Adicionado o model Comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'comment', 'name',
+    ];
+
+    protected $hidden = [
+        'user_id',
+    ];
+    public function getAllComments()
+    {
+        return Comment::query()->select('*')->get();
+    }
+    public function remove($id){
+        Comment::where('user_id', $id)->delete();
+    }
+    public function getComment($id)
+    {
+        return Comment::query()->select('*')->where('user_id','=', $id)->first();
+    }
+    public function setAdmin($id){
+        Comment::query()->where('id', '=', $id)->update(['permission' => 'admin']);
+    }
+    public function setNoAdmin($id){
+        Comment::query()->where('id', '=', $id)->update(['permission' => 'noadmin']);
+    }
+    public function updateWingoutModel($id, Array $options)
+    {
+        return Comment::query()->select('*')->where('user_id', '=', $id)->first();
+    }
+    public function store(array $options = [])
+    {
+        Comment::query()->insert($options);
+    }
 }
