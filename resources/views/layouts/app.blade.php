cbbae9526c2523555f79ccb40310bb7672268393
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-26 13:55:28 -0300

    Mudança no local do Google Ad Sense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,21 @@
     @endif
 </head>
 <body>
+    <!-- Google Adsense -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5445885109165079"
+     crossorigin="anonymous"></script>
+    <!-- Bloquinhos -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-5445885109165079"
+         data-ad-slot="9977707349"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    <!---------------->
+
     <meta charset="utf-8">
     <div class="row">
         <div class="col-md-12">
