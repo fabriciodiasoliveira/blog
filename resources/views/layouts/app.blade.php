66c7f9fc8809b71c43ad897f1de7ac2468213a63
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-26 17:40:53 -0300

    Teste com adsense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <meta charset="utf-8">
     <div class="row">
         <div class="col-md-12">
-             <iframe width="100%" src="{{ url('/') }}/carrousel" title=""></iframe>
+             {{-- <iframe width="100%" src="{{ url('/') }}/carrousel" title=""></iframe> --}}
         </div>
     </div>
     <div id="app">
