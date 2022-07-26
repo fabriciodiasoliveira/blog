afb8c21d201deb27b9b6cfd294a61327b90ddbb1
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-26 14:10:36 -0300

    Mudança no local do Google Ad Sense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,19 +11,6 @@
     <!-- Google Adsense -->
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5445885109165079"
      crossorigin="anonymous"></script>
-     
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5445885109165079"
-     crossorigin="anonymous"></script>
-    <!-- Bloquinhos -->
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-client="ca-pub-5445885109165079"
-         data-ad-slot="9977707349"
-         data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
-    <script>
-         (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
     <!---------------->
 
     <meta charset="utf-8">
@@ -95,6 +82,22 @@
             </div>
         </nav>
         <main class="py-4">
+            <div class="row">
+                <div class="col-md-12">
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5445885109165079"
+                     crossorigin="anonymous"></script>
+                    <!-- Bloquinhos -->
+                    <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-5445885109165079"
+                         data-ad-slot="9977707349"
+                         data-ad-format="auto"
+                         data-full-width-responsive="true"></ins>
+                    <script>
+                         (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-md-2">
                     @component('links')@endcomponent
