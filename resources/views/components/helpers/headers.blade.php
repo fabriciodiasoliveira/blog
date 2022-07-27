b4f1e58492b54a77683c6f34b34e9d9ab1860acd
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-27 12:33:49 -0300

    Preparação para a reversão do Adsense

--- resources/views/components/helpers/headers.blade.php
+++ resources/views/components/helpers/headers.blade.php
@@ -1,4 +1,5 @@
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
     
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
