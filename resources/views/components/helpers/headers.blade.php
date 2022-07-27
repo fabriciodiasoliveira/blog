3eaba06c127ba9c72ecaeef87b31a85164492838
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2022-07-27 11:15:45 -0300

    Removido o Adsense - testando outras tecnologias

--- resources/views/components/helpers/headers.blade.php
+++ resources/views/components/helpers/headers.blade.php
@@ -1,8 +1,3 @@
-    <!-- Google Adsense -->
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5445885109165079"
-     crossorigin="anonymous"></script>
-    <!---------------->
-    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <!-- CSRF Token -->
